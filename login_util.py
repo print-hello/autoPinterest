9e40fb2bf5d47d81551a0b76dff169bff3f82a27
Vinter Wang <printhello@163.com>
2020-07-20 17:38:05 +0800

    登录判断优化

--- login_util.py
+++ login_util.py
@@ -22,37 +22,35 @@ def login(driver, login_url, main_url, account_id, email, pwd, cookies):
             return login_state
         else:
             input_email_XP = '//input[@id="email"]'
-            input_email_flag = explicit_wait(driver, "VOEL", [input_email_XP, "XPath"], 15, False)
+            input_email_flag = explicit_wait(driver, "VOEL", [input_email_XP, "XPath"], 20, False)
             if input_email_flag:
                 driver.find_element_by_xpath(input_email_XP).send_keys(email)
                 time.sleep(1)
                 driver.find_element_by_name("password").send_keys(pwd)
                 time.sleep(1)
                 driver.find_element_by_xpath("//form//button").click()
-                login_success_XP = '//button[@aria-label="Messages"]'
-                login_success_flag = explicit_wait(driver, "VOEL", [login_success_XP, "XPath"], 30, False)
-                if login_success_flag:
-                    login_state = 11
-                    print('Account login successful!')
-                else:
-                    reset_passwd_XP = '//button[@aria-label="Reset password"]'
-                    reset_passwd_flag = explicit_wait(driver, "VOEL", [reset_passwd_XP, "XPath"], 5, False)
-                    if reset_passwd_flag:
-                        print('Error code: 9')
-                        login_state = 9
-                    elif driver.page_source.find('Reset your password') > -1:
-                        print('Error code: 9')
-                        login_state = 9
-                    elif driver.page_source.find('The email you entered does not belong to any account.') > -1:
-                        print('Error code: 66')
-                        login_state = 66
-                    elif driver.page_source.find('Your account has been suspended') > -1:
-                        print('Error code: 99')
-                        login_state = 99
-                    else:
-                        login_state = 0
+            login_success_XP = '//button[@aria-label="Messages"]'
+            login_success_flag = explicit_wait(driver, "VOEL", [login_success_XP, "XPath"], 30, False)
+            if login_success_flag:
+                login_state = 11
+                print('Account login successful!')
             else:
-                login_state = 0
+                reset_passwd_XP = '//button[@aria-label="Reset password"]'
+                reset_passwd_flag = explicit_wait(driver, "VOEL", [reset_passwd_XP, "XPath"], 5, False)
+                if reset_passwd_flag:
+                    print('Error code: 9')
+                    login_state = 9
+                elif driver.page_source.find('Reset your password') > -1:
+                    print('Error code: 9')
+                    login_state = 9
+                elif driver.page_source.find('The email you entered does not belong to any account.') > -1:
+                    print('Error code: 66')
+                    login_state = 66
+                elif driver.page_source.find('Your account has been suspended') > -1:
+                    print('Error code: 99')
+                    login_state = 99
+                else:
+                    login_state = 0
 
     return login_state
 
